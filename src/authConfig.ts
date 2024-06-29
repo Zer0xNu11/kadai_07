9b19cecfe207f81e928e0d4d2e56f9ad53d6aeb1
Zer0xNu11 <zerfnult@gmail.com>
2024-06-29 16:23:39 +0900

    register-rep2

--- src/authConfig.ts
+++ src/authConfig.ts
@@ -13,45 +13,49 @@ export const authConfig= {
   session:{strategy: 'jwt'}, //JWT利用によりサーバーメモリ節約
   callbacks: { 
 
-    async signIn({ user, account, profile, email, credentials }) {
-      const isAllowedToSignIn = true
-      if (isAllowedToSignIn) {
-        return true
-      } else {
-        // Return false to display a default error message
-        return false
-        // Or you can return a URL to redirect to:
-        // return '/unauthorized'
-      }
-    },
+    // async signIn({ user, account, profile, email, credentials }) {
+    //   const isAllowedToSignIn = true
+      
+    //   if (isAllowedToSignIn) {
+    //     return true
+    //   } else {
+    //     // Return false to display a default error message
+    //     return false
+    //     // Or you can return a URL to redirect to:
+    //     // return '/unauthorized'
+    //   }
+    // },
 
-    async redirect({ url, baseUrl }) {
-      // Allows relative callback URLs
-      if (url.startsWith("/")) return `${baseUrl}${url}`
-      // Allows callback URLs on the same origin
-      else if (new URL(url).origin === baseUrl) return url
-      return baseUrl
-    },
+    // async redirect({ url, baseUrl }) {
+    //   // Allows relative callback URLs
+    //   if (url.startsWith("/")) return `${baseUrl}${url}`
+    //   // Allows callback URLs on the same origin
+    //   else if (new URL(url).origin === baseUrl) return url
+    //   return baseUrl
+    // },
 
     authorized({request, auth}){ //認証前後の制約について設定する request情報 auth認証情報オブジェクト 
       const isLoggedIn = !!auth?.user; //ログインしているかどうかチェック
       const isAuthRoute = authRoutes.includes(request.nextUrl.pathname);
       const isPublicRoute = publicRoutes.includes(request.nextUrl.pathname);
 
-       //ルートフォルダ以外のページにログインしていない時アクセスすると拒否
-         if (!isPublicRoute && !isLoggedIn) {
-          return false;
-        }
+
 
       //ログイン、レジスターのサイト訪問時に、
       if (isAuthRoute) {
         //ログイン済みの時リダイレクト
         if (isLoggedIn) {
-          return Response.redirect(new URL('mypage', request.nextUrl));
+          return Response.redirect(new URL(DEFAULT_LOGIN_REDIRECT, request.nextUrl));
         }
 
         return true;
       }
+
+      //ルートフォルダ以外のページにログインしていない時アクセスすると拒否
+      if (!isPublicRoute && !isLoggedIn) {
+      return false;
+    }
+    return true;
 
       
 
